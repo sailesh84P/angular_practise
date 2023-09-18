6215bea70403b511a984fd9c189de627e2edae04
Sailesh Paul <sailesh.paul@technipfmc.com>
2023-09-18 17:51:58 +0200

    added title with babylon-js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularPractise';
+  title = 'BabylonJS with Angular';
   subtitle = "Practise for BabylonJS";
 }
